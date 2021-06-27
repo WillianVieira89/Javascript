908991ee493378e9288584ad4d9d77aec67080c6
Willian Leonardo Vieira <wilianleonardo@hotmail.com>
2021-06-27 01:07:48 -0300

    .
    commit inicial

---
+++ js/main.js
@@ -0,0 +1,135 @@
+/*
+function clicou(){
+    document.getElementById("agradecimento").innerHTML = "<b>Obrigado por clicar</b>";
+}
+
+function redirecionar(){
+    window.open("https://globallabs.academy/");
+    window.location.href = "https://globallabs.academy/";
+}
+
+function trocar(elemento){
+    elemento.innerHTML = "Obrigado por passar o mouse";
+    //document.getElementById("mouse").innerHTML = "Obrigado por passar o mouse";
+    //alert("Trocar texto");
+}
+
+
+function voltar(elemento){
+    elemento.innerHTML = "Passe o mouse aqui";
+    //document.getElementById("mouse").innerHTML = "Passe o mouse aqui";
+}
+
+function load(){
+    alert("pagina carregada")
+}
+
+function funcaoChange(elemento){
+    console.log(elemento.value)
+}*/
+
+/*
+function soma(n1, n2){
+    return n1 + n2;
+}
+*/
+
+
+/*
+function setReplace(frase, nome, novo_nome){
+    return frase.replace(nome, novo_nome)
+}
+
+alert(soma(5, 10));
+alert(setReplace("Vai Japão", "Japão", "Brasil"));
+*/
+
+
+/*
+function validaIdade(idade){
+    var validar;
+    if (idade >=18){
+        validar = true
+    }else {
+        validar = false
+    }
+    return validar;
+}
+*/
+
+
+/*
+var idade = prompt("Qual a sua idade");
+console.log(validaIdade(idade));
+*/
+
+
+/*
+var d = new Date;
+alert(d);
+alert(d.getHours());
+alert(d.getUTCDate())
+alert(d.getHours());
+alert(d.getFullYear())
+*/
+
+
+/*
+var count;
+for(count=0 <=5; count++){
+    alert(count);
+}
+*/
+
+
+/*
+var count=0;
+while (count <= 5){
+    console.log(count)
+    alert(count);
+    count++
+};
+*/
+
+
+/*
+var idade = prompt("Qual sua idade");
+if (idade >= 18){
+    alert("maior de idade");
+}else{
+    alert("menor de idade")
+};
+/*
+
+
+/*
+var frutas = [{nome: "maça", cor:"vermelha"}, {nome:"uva", cor:"roxa"}]
+console.log(frutas.nome);
+alert(frutas[1].cor);
+*/
+
+
+/*var fruta = {nome: "maça", cor:"vermelha"}
+console.log(fruta.nome);
+alert(fruta.cor);*/
+
+
+//var lista = ["maça", "pera", "laranja"];
+//lista.push("uva");
+//lista.pop();
+
+//console.log(lista);
+//console.log(lista.toString());
+//console.log(lista.join(" - "));
+
+
+//var nome = "Willian Leonardo Vieira";
+//var n1 = 5
+//var n2 = 3
+//var frase = "Japão é o melhor time do mundo"
+//alert(nome + " tem " + idade + " anos");
+//alert (idade1 + idade2);
+//console.log(nome);
+//console.log(n1+n2);
+//console.log(frase.toLowerCase());
+//console.log(frase.replace("Japão", "Brasil"))
